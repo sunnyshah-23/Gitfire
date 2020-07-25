3fe4989a90a59be870ac6b4cb60004a5cdd159ee
Sunny shah <51815549+sunnyshah-23@users.noreply.github.com>
2020-07-25 16:45:23 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,4 @@
 export default {
-    apiKey: "AIzaSyBoM645bfw6Ij-lTbim_mKXIuwjfyiWKLg",
-    authDomain: "mygitapp-12b2b.firebaseapp.com",
-    databaseURL: "https://mygitapp-12b2b.firebaseio.com",
-    projectId: "mygitapp-12b2b",
-    storageBucket: "mygitapp-12b2b.appspot.com",
-    messagingSenderId: "253237538881",
-    appId: "1:253237538881:web:b84d8c54dc78060514d6c0",
-    measurementId: "G-8LHJNSE6WV"
+    //get your configuration of firebase
+   
   };
